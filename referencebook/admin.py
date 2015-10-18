50402421b47cf3cee6870ad624cd6d08fee4131c
Scherbak Alexander A <scherbak.alexander@gmail.com>
2015-10-18 20:28:28 +0300

    Added editable values in cartridges and models of printers

--- referencebook/admin.py
+++ referencebook/admin.py
@@ -13,11 +13,13 @@ from .models import Connectivity
 
 class ModelList(admin.ModelAdmin):
     list_display = ('model_name', 'product_number', 'cartridges_list', 'print_type', 'max_monthly_load',
-                    'max_paper_size')
+                    'max_paper_size', 'duplex_mode')
+    list_editable = ('product_number', 'print_type', 'max_monthly_load', 'max_paper_size', 'duplex_mode')
 
 
 class CartridgeList(admin.ModelAdmin):
     list_display = ('cartridge_name', 'color_type', 'max_number_prints', 'package', 'printers_list')
+    list_editable = ('color_type', 'max_number_prints', 'package')
 
 admin.site.register(PrinterModel, ModelList)
 admin.site.register(PrinterType)
